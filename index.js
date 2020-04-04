8c4042016289e7c24c410a2b80307ed383c9495c
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 11:22:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function nowServing(line){
   if (line.length > 0){
     
   }
+  else{
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(line){
+  let position = 1
+  var newLine = ""
+  if (line.length > 0){
+    while (position >= line.length){
+      newLine += `${position}. ${line[position-1]}
+    }
+  }
 }
