853a77e586d3e10f4398cf427667a9e7e56fce09
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 11:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line){
   var newLine = "The line is currently: "
   if (line.length > 0){
     while (position < line.length){
-      newLine += `${position}. ${line[position-1]},`
+      newLine += `${position}. ${line[position-1]}, `
       position++
     }
     newLine += `${position}. ${line[position-1]}`
